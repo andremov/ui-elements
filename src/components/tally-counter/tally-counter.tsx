2dfb683645e90d841a7fdb43789f73e74dafabca
Andres-XL <amovillaw01@gmail.com>
2024-04-26 21:55:00 -0500

    fix dark mode issue with tallies

--- src/components/tally-counter/tally-counter.tsx
+++ src/components/tally-counter/tally-counter.tsx
@@ -5,32 +5,39 @@ interface TallyCounterProps {
    * Size of tallies (in px)
    */
   size?: number;
-
   /**
    * Number to count to (greater than or equal to 0)
    */
   count?: number;
-
   /**
    * Whether to display the count LTR or RTL
    */
   reverse?: boolean;
+  /**
+   * Color of the tally icons
+   */
+  tallyColor?: 'text-white' | 'text-black';
 }
 
-export function TallyCounter({ count = 7, reverse = false, size = 50 }: TallyCounterProps) {
+export function TallyCounter({
+  count = 7,
+  reverse = false,
+  size = 50,
+  tallyColor = 'text-black',
+}: TallyCounterProps) {
   const fullTallies = Math.floor(count / 5);
   const remTallies = count - fullTallies * 5;
 
-  if (count === 0) return <span>0</span>;
-  if (count > 10) return <span>{count}</span>;
+  if (count === 0) return <span className={tallyColor}>0</span>;
+  if (count > 10) return <span className={tallyColor}>{count}</span>;
 
   return (
     <span className={`flex gap-0.5 ${reverse ? 'flex-row-reverse' : ''}`}>
       {[...Array(fullTallies).keys()].map((item) => (
-        <TallyIcon size={size} count={5} key={item} />
+        <TallyIcon size={size} count={5} key={item} tallyColor={tallyColor} />
       ))}
       <div style={reverse ? { transform: 'rotateY(180deg)' } : {}}>
-        <TallyIcon size={size} count={remTallies} />
+        <TallyIcon size={size} count={remTallies} tallyColor={tallyColor} />
       </div>
     </span>
   );
